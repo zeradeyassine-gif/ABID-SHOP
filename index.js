e2b8a44da914775a9909f4f6a073c533276d1a97
zeradeyassine-gif <zeradeyassine@gmail.com>
2026-05-10 08:41:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const {
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const pino = require("pino");
 const fs = require("fs-extra");
+const crypto = require("crypto");
 const dotenv = require("dotenv");
 
 dotenv.config();
